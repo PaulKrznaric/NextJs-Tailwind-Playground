5bc04314e71bdeeea0cb3cd5ccdc2e1e882d7707
PaulKrznaric <paulkrznaric@gmail.com>
2022-11-30 16:37:02 -0500

    Added return to home option on Contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,12 +1,22 @@
 // pages/contact.tsx
 
 import SimpleForm from "../components/Form/SimpleForm";
+import SubmitButton from "../components/Form/SubmitButton";
 
 export default function Contact() {
   return (
-    <div>
-      <h1 className="bold text-4xl text-center"> Form </h1>
+    <div className="flex flex-col items-center">
+      <h1 className="bold text-4xl text-center p-4"> Contact Us </h1>
       <SimpleForm buttonText={"Submit"} onSubmit={undefined}></SimpleForm>
+      <SubmitButton
+        className="w-40"
+        onClick={() => {
+          window.location.href = "/";
+        }}
+        size="36"
+      >
+        Return To Home
+      </SubmitButton>
     </div>
   );
 }
